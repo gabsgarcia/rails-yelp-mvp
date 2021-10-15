053fc32f96a61f8bfa5e1f159b1b8b7f61af8434
gabriella garcia <gabirvgarcia@hotmail.com>
2021-10-14 22:08:33 -0300

    reviews create working

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,3 +1,27 @@
 class ReviewsController < ApplicationController
 
+  def new
+    # we need @restaurant in our `simple_form_for`
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+  end
+
+  # A visitor can add a new review to a restaurant
+  def create
+    @review = Review.new(review_params) # GET POST
+    # we need `restaurant_id` to associate review with corresponding restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review.restaurant = @restaurant
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
 end
