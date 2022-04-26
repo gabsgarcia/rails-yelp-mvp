929f48bed1e492235405327800631e9c979b455e
gabriella garcia <gabirvgarcia@hotmail.com>
2022-04-26 18:40:55 -0300

    rails new

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,3 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  resources :restaurants, only:[:index, :new, :create, :show] do
-    resources :reviews, only:[:new, :create, :show]
-  end
-
- # get 'restaurants/new', to: 'restaurants#new'
- # get 'restaurants', to: 'restaurants#index'
- # get 'restaurants/:id', to: 'restaurants#show'
-
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,7 @@
 # This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-require "faker"
-category = %w[chinese italian japanese french belgian]
-
-# TODO: Write a seed to insert 5 restaurants in the database
-puts "Cleaning database..."
-Restaurant.destroy_all
-puts "Done..."
-puts 'Creating 5 fake restaurants...'
-5.times do
-  restaurant = Restaurant.new(
-    name: Faker::Restaurant.name,
-    address: Faker::Address.street_address,
-    category: category.sample
-  )
-  restaurant.save!
-end
-puts 'Finished!'
